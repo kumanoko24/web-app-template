0a7aa7ca6ceafc1c84eb3533019072ada857ea0d
kumanoko <noeleon930.hater.1@gmail.com>
2019-08-13 02:39:38 +0800

    Update dev.webpack.config.js

--- dev.webpack.config.js
+++ dev.webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
   },
   devtool: "inline-source-map",
   devServer: {
-    contentBase: path.join(__dirname, "dist"),
+    contentBase: path.join(__dirname, "src"),
     hot: true,
     watchContentBase: true,
     port: 9000
